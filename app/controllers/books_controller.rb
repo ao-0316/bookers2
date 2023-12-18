a81ea27761be3084171b41aa85a5b0c1916693ec
ao-0316 <aosho0316.yukiusagi@gmail.com>
2023-12-18 05:54:59 +0000

    4-5

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -26,24 +26,32 @@ class BooksController < ApplicationController
   
 
   def show
+    @book = Book.new
+    @books = Book.all
   end
   
   def edit
+    @book = Book.find(params[:id])
+    @user = @book.user
+    unless @user.id == current_user.id
+    redirect_to book_path  
+    end
     
-    book = Book.find(params[:id])
   end
 
+  def update
+    book = Book.find(params[:id])
+    book.update(book_params)
+    redirect_to book_path(book.id)  
+  end
+  
   def destroy
     book = Book.find(params[:id])
     book.destroy
     redirect_to '/books'
   end
   
-  def update
-    book = Book.find(params[:id])
-    book.update(book_params)
-    redirect_to book_path(book.id)  
-  end
+  
   
   
   
